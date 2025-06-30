ff3da3f2cd5fc5ae45fcbc24d280b9dc1a3c53a2
Nikitka <nik.nikitosa@mail.ru>
2025-06-30 21:23:26 +0300

    75 commit

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -4,5 +4,5 @@ from .views import short_link_redirect
 
 
 urlpatterns = [
-    path('/s/<int:recipe_id>/', short_link_redirect, name='recipe-short-link'),
+    path('s/<int:recipe_id>/', short_link_redirect, name='recipe-short-link'),
 ]
