24ce064d5f224dc79a7acaec5ca41f11f2bdd5a3
Nikitka <nik.nikitosa@mail.ru>
2025-06-30 17:55:09 +0300

    67 commit

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import redirect
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from .models import Recipe
+from .serializers import RecipeSerializer
 
 
 def short_link_redirect(request, recipe_id):
     if not Recipe.objects.filter(pk=recipe_id).exists():
         raise ValidationError(f'Рецепт с id {recipe_id} не найден.')
-    return redirect(f'/api/recipes/{recipe_id}/')
+    recipe = Recipe.objects.get(pk=recipe_id)
+    serializer = RecipeSerializer(recipe, context={'request': request})
+    return Response(serializer.data, status=status.HTTP_200_OK)
