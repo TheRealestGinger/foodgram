d3356bd138a68dec52bdb405cfacc5b1c4af1bab
Nikitka <nik.nikitosa@mail.ru>
2025-06-30 21:12:02 +0300

    73 commit

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -7,4 +7,4 @@ from .models import Recipe
 def short_link_redirect(request, recipe_id):
     if not Recipe.objects.filter(pk=recipe_id).exists():
         raise ValidationError(f'Рецепт с id {recipe_id} не найден.')
-    return redirect(f'http://cleza.hopto.org/recipes/{recipe_id}/')
+    return redirect(f'/api/recipes/{recipe_id}/')
