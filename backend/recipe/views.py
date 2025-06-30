97146bc3cf08c266932b2b7fe3b71530b7888eec
Nikitka <nik.nikitosa@mail.ru>
2025-06-30 21:48:36 +0300

    76 commit

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import redirect
-from rest_framework.exceptions import ValidationError
+from django.http import Http404
 
 from .models import Recipe
 
 
 def short_link_redirect(request, recipe_id):
     if not Recipe.objects.filter(pk=recipe_id).exists():
-        raise ValidationError(f'Рецепт с id {recipe_id} не найден.')
-    return redirect(f'recipes/{recipe_id}/')
+        raise Http404(f'Рецепт с id {recipe_id} не найден.')
+    return redirect(f'/recipes/{recipe_id}/')
