5f416e3224376873f3dc91d8da35c28db4e44cb4
Nikitka <nik.nikitosa@mail.ru>
2025-06-30 17:38:44 +0300

    66 commit

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,11 +1,9 @@
-from django.views.decorators.cache import never_cache
 from django.shortcuts import redirect
 from rest_framework.exceptions import ValidationError
 
 from .models import Recipe
 
 
-@never_cache
 def short_link_redirect(request, recipe_id):
     if not Recipe.objects.filter(pk=recipe_id).exists():
         raise ValidationError(f'Рецепт с id {recipe_id} не найден.')
