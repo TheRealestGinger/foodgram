9b27a98f08f1eea1f79bbcb22e0ab6f18112a49b
Nikitka <nik.nikitosa@mail.ru>
2025-06-30 20:53:46 +0300

    71 commit

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from .views import short_link_redirect
 
 
-namespace = 'recipe'
-
 urlpatterns = [
     path('s/<int:recipe_id>/', short_link_redirect, name='recipe-short-link'),
 ]
